c17ce37edfc3b4f25156386ffb52aa319e7ced58
Shruti Tiwari <sh03shruti@gmail.com>
2021-04-28 00:39:14 +0530

    Added login route

---
+++ controllers/loginController.js
@@ -0,0 +1,23 @@
+const User = require('../schema/userSchema');
+
+module.exports = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		res.status(401);
+	} else if (
+		!(req.body.email && req.body.password) ||
+		req.body.email == '' ||
+		req.body.password == ''
+	) {
+		res.send({
+			message: 'Incorrect Email Address or Password',
+			status: 'failed'
+		});
+	} else {
+		const user = new User({
+			email: req.body.email,
+			password: req.body.password
+		});
+		res.locals.user = user;
+		next();
+	}
+};
