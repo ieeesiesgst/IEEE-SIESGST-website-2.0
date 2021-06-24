d1a82f97273158131f950f46a714f34444b702cc
Shruti Tiwari <sh03shruti@gmail.com>
2021-06-24 15:30:31 +0530

    backend link

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -2,14 +2,19 @@ const axios = require('axios');
 const qs = require('qs');
 
 module.exports = function callAppScript(domainData) {
+	console.log(domainData);
 	return new Promise(function (resolve, reject) {
 		axios
 			.post(process.env.GET_SCRIPT_DATA, qs.stringify(domainData))
 			.then((response) => {
-				console.log(response);
+				// handle success
+				console.log('working', response.data);
+				resolve(1);
 			})
-			.catch((err) => {
-				console.log(err);
+			.catch(function (error) {
+				// handle error
+				console.log('hereeeeee', error);
+				resolve(2);
 			});
 	});
 };
--- src/routes/generalRouter.js
+++ src/routes/generalRouter.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const getData = require('../functions/getData');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 	if (req.isAuthenticated()) {
 		res.send(req.user);
 	} else {
+		const domainData = {
+			domain: 'events',
+			topic: 'events'
+		};
+		var x = await getData(domainData);
+		console.log(x);
+		// res.send('correct');
 		res.render('test', { title: 'HOME | IEEE SIESGST' });
 	}
 });
