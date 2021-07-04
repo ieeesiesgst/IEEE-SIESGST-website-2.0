b2c76a5e542f9edd0d6718c295ac022cac61a1cb
Shruti Tiwari <sh03shruti@gmail.com>
2021-07-04 23:37:05 +0530

    gallery data/ nav link

--- src/routes/galleryRouter.js
+++ src/routes/galleryRouter.js
@@ -1,10 +1,16 @@
 const express = require('express');
-// const getData = require('../functions/getData');
+const getData = require('../functions/getData');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 	try {
+		const domainData = {
+			domain: 'gallery',
+			topic: 'gallery'
+		};
+		const galleryRes = await getData(domainData);
+		// console.log(galleryRes.data);
 		res.render('gallery', {
 			title: 'GALLERY | IEEE SIESGST'
 		});
