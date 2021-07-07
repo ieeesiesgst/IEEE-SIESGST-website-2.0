5bb46a6c3ffe93ce3f80b46564d95bdab955fbb5
Shruti Tiwari <sh03shruti@gmail.com>
2021-07-07 14:05:36 +0530

    app script pagination

--- src/routes/galleryRouter.js
+++ src/routes/galleryRouter.js
@@ -8,43 +8,20 @@ router.get('/', async (req, res) => {
 	try {
 		const domainData = {
 			domain: 'gallery',
-			topic: 'gallery'
+			topic: 'gallery',
+			page: req.query.page
 		};
 		const galleryRes = await getData(domainData);
 		const Gdata = galleryRes.data;
-		// console.log(Gdata.Gname);
+		// Gdata -- object of arrays i.e name, imgLink, description, type
+		// console.log(Gdata);
 
-		const lastPg = Math.ceil(Gdata.Gname.length / 9);
-		// const limit = 9;
-		let page;
-
-		if (req.query.page * 1 > lastPg) {
-			page = lastPg;
-		} else if (req.query.page * 1 <= lastPg && req.query.page * 1 > 0) {
-			page = req.query.page * 1;
-		} else {
-			page = 1;
-		}
-
-		const skip = (page - 1) * 9;
-		let imgName = [],
-			imgDes = [],
-			imgLink = [];
-
-		for (let i = skip; i < skip + 9; i++) {
-			if (Gdata.Gname[i] != undefined) {
-				imgName.push(Gdata.Gname[i]);
-			}
-			if (Gdata.Gdes[i] != undefined) {
-				imgDes.push(Gdata.Gdes[i]);
-			}
-			if (Gdata.Gimg[i] != undefined) {
-				imgLink.push(Gdata.Gimg[i]);
-			}
-		}
-		// console.log(imgName);
-		// console.log(imgDes);
-		// console.log(imgLink);
+		console.log(Gdata.Gname);
+		// console.log(Gdata.Gdes);
+		// console.log(Gdata.Gimg);
+		// console.log(Gdata.Gtype);
+		// console.log(Gdata.Galt);
+		// console.log(Gdata.GlastPg);
 
 		res.render('gallery', {
 			title: 'GALLERY | IEEE SIESGST'
