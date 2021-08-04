2d4d30cd9bccca379ead0590c4ddbe6506e4c395
Shruti Tiwari <sh03shruti@gmail.com>
2021-08-05 00:11:50 +0530

    member data

--- src/routes/memberRouter.js
+++ src/routes/memberRouter.js
@@ -10,8 +10,47 @@ router.get('/', async (req, res) => {
 		};
 		const teamRes = await getData(domainData);
 		// console.log(teamRes.data);
+		const teamName = req.query.team;
+		let memberRes;
+
+		if (req.query.team == 'core') {
+			memberRes = teamRes.data.core;
+		} else if (req.query.team == 'faculty') {
+			memberRes = teamRes.data.faculty;
+		} else if (req.query.team == 'wie') {
+			memberRes = teamRes.data.wie;
+		} else if (req.query.team == 'cs') {
+			memberRes = teamRes.data.cs;
+		} else if (req.query.team == 'mtts') {
+			memberRes = teamRes.data.mtts;
+		} else if (req.query.team == 'isv') {
+			memberRes = teamRes.data.isv;
+		} else if (req.query.team == 'jsec') {
+			memberRes = teamRes.data.jsec;
+		} else if (req.query.team == 'tech') {
+			memberRes = teamRes.data.tech;
+		} else if (req.query.team == 'admin') {
+			memberRes = teamRes.data.admin;
+		} else if (req.query.team == 'pr') {
+			memberRes = teamRes.data.pr;
+		} else if (req.query.team == 'media') {
+			memberRes = teamRes.data.media;
+		} else if (req.query.team == 'publicity') {
+			memberRes = teamRes.data.publicity;
+		} else if (req.query.team == 'design') {
+			memberRes = teamRes.data.design;
+		} else if (req.query.team == 'creative') {
+			memberRes = teamRes.data.creative;
+		} else if (req.query.team == 'mdo') {
+			memberRes = teamRes.data.mdo;
+		} else if (req.query.team == 'treasurer') {
+			memberRes = teamRes.data.treasurer;
+		} else {
+			memberRes = teamRes.data.core;
+		}
 		res.render('members', {
-			title: 'TEAM | IEEE SIESGST'
+			title: 'TEAM | IEEE SIESGST',
+			memberRes: memberRes
 		});
 	} catch (err) {
 		console.log(err);
