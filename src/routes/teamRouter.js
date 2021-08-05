ab275ae3a9b5bef602e4d0a9e2728157418265ad
Shruti Tiwari <sh03shruti@gmail.com>
2021-08-06 00:05:24 +0530

    team-members linked

--- src/routes/teamRouter.js
+++ src/routes/teamRouter.js
@@ -1,15 +1,9 @@
 const express = require('express');
-// const getData = require('../functions/getData');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	try {
-		// const domainData = {
-		// 	domain: 'team',
-		// 	topic: 'team'
-		// };
-		// const homeResponse = await getData(domainData);
 		res.render('teams', {
 			title: 'TEAM | IEEE SIESGST'
 		});
