64c07a55d45fdc9a880c78668bfaf89c9e21e789
Shruti Tiwari <sh03shruti@gmail.com>
2021-08-07 22:44:15 +0530

    member error handling

--- src/routes/memberRouter.js
+++ src/routes/memberRouter.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const getData = require('../functions/getData');
 const cachedata = require('../cache/cacheData');
 
 const router = express.Router();
@@ -9,45 +8,52 @@ router.get('/', (req, res) => {
 		const teamRes = cachedata('teams');
 		let memberRes;
 
-		if (req.query.team == 'core') {
-			memberRes = teamRes.core;
-		} else if (req.query.team == 'faculty') {
-			memberRes = teamRes.faculty;
-		} else if (req.query.team == 'wie') {
-			memberRes = teamRes.wie;
-		} else if (req.query.team == 'cs') {
-			memberRes = teamRes.cs;
-		} else if (req.query.team == 'mtts') {
-			memberRes = teamRes.mtts;
-		} else if (req.query.team == 'isv') {
-			memberRes = teamRes.isv;
-		} else if (req.query.team == 'jsec') {
-			memberRes = teamRes.jsec;
-		} else if (req.query.team == 'tech') {
-			memberRes = teamRes.tech;
-		} else if (req.query.team == 'admin') {
-			memberRes = teamRes.admin;
-		} else if (req.query.team == 'pr') {
-			memberRes = teamRes.pr;
-		} else if (req.query.team == 'media') {
-			memberRes = teamRes.media;
-		} else if (req.query.team == 'publicity') {
-			memberRes = teamRes.publicity;
-		} else if (req.query.team == 'design') {
-			memberRes = teamRes.design;
-		} else if (req.query.team == 'creative') {
-			memberRes = teamRes.creative;
-		} else if (req.query.team == 'mdo') {
-			memberRes = teamRes.mdo;
-		} else if (req.query.team == 'treasurer') {
-			memberRes = teamRes.treasurer;
+		if (teamRes.Error) {
+			res.status(500).json({
+				status: 'Fail',
+				message: 'Server Error!'
+			});
 		} else {
-			memberRes = teamRes.core;
+			if (req.query.team == 'core') {
+				memberRes = teamRes.core;
+			} else if (req.query.team == 'faculty') {
+				memberRes = teamRes.faculty;
+			} else if (req.query.team == 'wie') {
+				memberRes = teamRes.wie;
+			} else if (req.query.team == 'cs') {
+				memberRes = teamRes.cs;
+			} else if (req.query.team == 'mtts') {
+				memberRes = teamRes.mtts;
+			} else if (req.query.team == 'isv') {
+				memberRes = teamRes.isv;
+			} else if (req.query.team == 'jsec') {
+				memberRes = teamRes.jsec;
+			} else if (req.query.team == 'tech') {
+				memberRes = teamRes.tech;
+			} else if (req.query.team == 'admin') {
+				memberRes = teamRes.admin;
+			} else if (req.query.team == 'pr') {
+				memberRes = teamRes.pr;
+			} else if (req.query.team == 'media') {
+				memberRes = teamRes.media;
+			} else if (req.query.team == 'publicity') {
+				memberRes = teamRes.publicity;
+			} else if (req.query.team == 'design') {
+				memberRes = teamRes.design;
+			} else if (req.query.team == 'creative') {
+				memberRes = teamRes.creative;
+			} else if (req.query.team == 'mdo') {
+				memberRes = teamRes.mdo;
+			} else if (req.query.team == 'treasurer') {
+				memberRes = teamRes.treasurer;
+			} else {
+				memberRes = teamRes.core;
+			}
+			res.render('members', {
+				title: 'TEAM MEMBERS | IEEE SIESGST',
+				memberRes: memberRes
+			});
 		}
-		res.render('members', {
-			title: 'TEAM MEMBERS | IEEE SIESGST',
-			memberRes: memberRes
-		});
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({
