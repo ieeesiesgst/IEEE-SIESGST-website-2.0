954f710714c0062aa9ed4951b9a094df59dfe067
Shruti Tiwari <sh03shruti@gmail.com>
2022-01-13 00:29:38 +0530

    decrypted message

--- src/routes/techopedia10.js
+++ src/routes/techopedia10.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const CryptoJS = require('crypto-js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	try {
+		let decryptName = CryptoJS.Rabbit.decrypt(
+			req.query.x,
+			'NsdvkbDIK23kjv0bNFvV349vbhK920bJ'
+		).toString(CryptoJS.enc.Utf8);
+
+		let decryptEvent = CryptoJS.Rabbit.decrypt(
+			req.query.y,
+			'NsdvkbDIK23kjv0bNFvV349vbhK920bJ'
+		).toString(CryptoJS.enc.Utf8);
+
+		console.log(decryptName, decryptEvent);
 		res.send('TechopediaX receipts route working.');
 	} catch (err) {
 		console.log(err);
