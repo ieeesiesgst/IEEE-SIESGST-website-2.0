2cbff6f408c2f58564c73c3214cdb803bb662d5e
Shruti Tiwari <sh03shruti@gmail.com>
2022-01-13 17:09:25 +0530

    all params

--- src/routes/techopedia10.js
+++ src/routes/techopedia10.js
@@ -3,19 +3,38 @@ const CryptoJS = require('crypto-js');
 
 const router = express.Router();
 
+function decryptValue(encodedVal) {
+	let decodedVal = [];
+	for (let k = 0; k < encodedVal.length; k++) {
+		let decryptName = CryptoJS.Rabbit.decrypt(
+			encodedVal[k],
+			process.env.TECHOEDIAX_KEY
+		).toString(CryptoJS.enc.Utf8);
+		decodedVal.push(decryptName);
+	}
+	return decodedVal;
+}
+
 router.get('/', (req, res) => {
 	try {
-		let decryptName = CryptoJS.Rabbit.decrypt(
-			req.query.x,
-			process.env.TECHOEDIAX_KEY
-		).toString(CryptoJS.enc.Utf8);
-
-		let decryptEvent = CryptoJS.Rabbit.decrypt(
-			req.query.y,
-			process.env.TECHOEDIAX_KEY
-		).toString(CryptoJS.enc.Utf8);
-
-		console.log(decryptName, decryptEvent);
+		let params = Object.keys(req.query).length;
+		let encodedValue;
+		if (params == 3) {
+			encodedValue = [req.query.x, req.query.a, req.query.b];
+		}
+		if (params == 4) {
+			encodedValue = [req.query.x, req.query.y, req.query.a, req.query.b];
+		}
+		if (params == 5) {
+			encodedValue = [
+				req.query.x,
+				req.query.y,
+				req.query.z,
+				req.query.a,
+				req.query.b
+			];
+		}
+		console.log(decryptValue(encodedValue));
 		res.send('TechopediaX receipts route working.');
 	} catch (err) {
 		console.log(err);
