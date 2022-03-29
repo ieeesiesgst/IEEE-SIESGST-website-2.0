2d3d16033b2e3cbe4c4edfb74946d8f0d94f8fff
Shruti Tiwari <sh03shruti@gmail.com>
2022-03-29 19:33:15 +0530

    update

--- src/schema/userSchema.js
+++ src/schema/userSchema.js
@@ -1,70 +1,70 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+// const mongoose = require('mongoose');
+// const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	college: {
-		type: String,
-		trim: true
-	},
-	branch: {
-		type: String,
-		trim: true
-	},
-	contact: {
-		type: String,
-		trim: true
-	},
-	googleId: {
-		type: String
-	},
-	microsoftId: {
-		type: String
-	},
-	verified: {
-		type: Boolean,
-		default: false
-	},
-	profilePic: {
-		type: String
-	},
-	password: {
-		type: String
-	},
-	profileDetails: {
-		type: Boolean,
-		default: false
-	}
-});
+// const userSchema = new mongoose.Schema({
+// 	name: {
+// 		type: String,
+// 		required: true,
+// 		trim: true
+// 	},
+// 	email: {
+// 		type: String,
+// 		required: true,
+// 		unique: true
+// 	},
+// 	college: {
+// 		type: String,
+// 		trim: true
+// 	},
+// 	branch: {
+// 		type: String,
+// 		trim: true
+// 	},
+// 	contact: {
+// 		type: String,
+// 		trim: true
+// 	},
+// 	googleId: {
+// 		type: String
+// 	},
+// 	microsoftId: {
+// 		type: String
+// 	},
+// 	verified: {
+// 		type: Boolean,
+// 		default: false
+// 	},
+// 	profilePic: {
+// 		type: String
+// 	},
+// 	password: {
+// 		type: String
+// 	},
+// 	profileDetails: {
+// 		type: Boolean,
+// 		default: false
+// 	}
+// });
 
-const options = {
-	usernameField: 'email',
-	usernameLowerCase: true,
-	limitAttempts: true,
-	maxAttempts: 5,
-	passwordValidator: (password, cb) => {
-		if (password.length < 8) {
-			return cb({
-				message: 'Password should have minimum 8 characters.'
-			});
-		} else {
-			return cb();
-		}
-	},
-	errorMessages: { NoSaltValueStoredError: 'Some error occurred.' }
-};
+// const options = {
+// 	usernameField: 'email',
+// 	usernameLowerCase: true,
+// 	limitAttempts: true,
+// 	maxAttempts: 5,
+// 	passwordValidator: (password, cb) => {
+// 		if (password.length < 8) {
+// 			return cb({
+// 				message: 'Password should have minimum 8 characters.'
+// 			});
+// 		} else {
+// 			return cb();
+// 		}
+// 	},
+// 	errorMessages: { NoSaltValueStoredError: 'Some error occurred.' }
+// };
 
-userSchema.plugin(passportLocalMongoose, options);
+// userSchema.plugin(passportLocalMongoose, options);
 
-const User = new mongoose.model('User', userSchema);
+// const User = new mongoose.model('User', userSchema);
 
-module.exports = User;
+// module.exports = User;
